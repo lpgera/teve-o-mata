f312fccb91e0eed31db277c0b9dd8e75fedf4b90
Gerencsér András <lpgera@gmail.com>
2017-02-18 21:30:21 +0100

    code formatting

--- teve-o-mata/src/index.js
+++ teve-o-mata/src/index.js
@@ -13,47 +13,41 @@ const teveList = config.get('teveclub')
 _.map(teveList, (teve) => {
   const teveclubRestClient = new TeveclubRestClient()
 
-  return teveclubRestClient.getAsync('/')
-    .then(() => {
-      return teveclubRestClient.postAsync('/', {
-        form: {
-          tevenev: teve.login,
-          pass: teve.password,
-          x: '34',
-          y: '33',
-          login: 'Gyere!',
-        },
-      })
+  return teveclubRestClient.getAsync('/').then(() => {
+    return teveclubRestClient.postAsync('/', {
+      form: {
+        tevenev: teve.login,
+        pass: teve.password,
+        x: '34',
+        y: '33',
+        login: 'Gyere!',
+      },
     })
-    .then(() => {
-      return teveclubRestClient.postAsync('/myteve.pet', {
-        form: {
-          kaja: 1,
-          pia: 1,
-          etet: 'Mehet!',
-        },
-      })
+  }).then(() => {
+    return teveclubRestClient.postAsync('/myteve.pet', {
+      form: {
+        kaja: 1,
+        pia: 1,
+        etet: 'Mehet!',
+      },
     })
-    .then(() => {
-      return teveclubRestClient.getAsync('/tanit.pet')
+  }).then(() => {
+    return teveclubRestClient.getAsync('/tanit.pet')
+  }).spread((response, body) => {
+    const $ = cheerio.load(body)
+    if ($('select').length > 2) {
+      return pusher.noteAsync(null, 'Teve-o-mata', `Válassz új trükköt ${teve.login} tevédnek!`)
+    }
+    return teveclubRestClient.postAsync('/tanit.pet', {
+      form: {
+        farmdoit: 'tanit',
+        learn: 'Tanulj teve!',
+      },
     })
-    .spread((response, body) => {
-      const $ = cheerio.load(body)
-      if ($('select').length > 2) {
-        return pusher.noteAsync(null, 'Teve-o-mata', `Válassz új trükköt ${teve.login} tevédnek!`)
-      }
-      return teveclubRestClient.postAsync('/tanit.pet', {
-        form: {
-          farmdoit: 'tanit',
-          learn: 'Tanulj teve!',
-        },
-      })
-    })
-    .then(() => {
-      return pusher.noteAsync(null, 'Teve-o-mata', `${teve.login} tanítva és megetetve!`)
-    })
-    .catch((err) => {
-      log.error(err)
-      return pusher.noteAsync(null, 'Teve-o-mata', err)
-    })
+  }).then(() => {
+    return pusher.noteAsync(null, 'Teve-o-mata', `${teve.login} tanítva és megetetve!`)
+  }).catch((err) => {
+    log.error(err)
+    return pusher.noteAsync(null, 'Teve-o-mata', err)
+  })
 })
