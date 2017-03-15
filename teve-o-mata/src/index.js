705f37bced3d564a6a4a404d802da812f9cbdf85
Gerencsér András <lpgera@gmail.com>
2017-03-15 10:08:04 +0100

    refactored each step into separate functions

--- teve-o-mata/src/index.js
+++ teve-o-mata/src/index.js
@@ -10,30 +10,34 @@ const pusher = Promise.promisifyAll(new Pushbullet(config.get('pushbullet.apiKey
 
 const teveList = config.get('teveclub')
 
-_.map(teveList, (teve) => {
-  const teveclubRestClient = new TeveclubRestClient()
+function init(teveclubRestClient) {
+  return teveclubRestClient.getAsync('/')
+}
 
-  return teveclubRestClient.getAsync('/').then(() => {
-    return teveclubRestClient.postAsync('/', {
-      form: {
-        tevenev: teve.login,
-        pass: teve.password,
-        x: '34',
-        y: '33',
-        login: 'Gyere!',
-      },
-    })
-  }).then(() => {
-    return teveclubRestClient.postAsync('/myteve.pet', {
-      form: {
-        kaja: 1,
-        pia: 1,
-        etet: 'Mehet!',
-      },
-    })
-  }).then(() => {
-    return teveclubRestClient.getAsync('/tanit.pet')
-  }).spread((response, body) => {
+function login(teveclubRestClient, teve) {
+  return teveclubRestClient.postAsync('/', {
+    form: {
+      tevenev: teve.login,
+      pass: teve.password,
+      x: '34',
+      y: '33',
+      login: 'Gyere!',
+    },
+  })
+}
+
+function feed(teveclubRestClient) {
+  return teveclubRestClient.postAsync('/myteve.pet', {
+    form: {
+      kaja: 1,
+      pia: 1,
+      etet: 'Mehet!',
+    },
+  })
+}
+
+function continueTeaching(teveclubRestClient) {
+  return teveclubRestClient.getAsync('/tanit.pet').spread((response, body) => {
     const $ = cheerio.load(body)
     if ($('select').length > 2) {
       return false
@@ -46,27 +50,48 @@ _.map(teveList, (teve) => {
     }).then(() => {
       return true
     })
-  }).then((tanitva) => {
-    if (tanitva) {
-      return teveclubRestClient.getAsync('/tanit.pet').spread((response, body) => {
-        const $ = cheerio.load(body)
-        const leckekRawText = $('tr:nth-child(1) > td > font > b > div').text()
-        const leckekMatches = leckekRawText.match(new RegExp('(\\d+)[^\\d]+Ebb.l tev.d m.r[^\\d]+(\\d+)'))
-        const osszesLecke = _.get(leckekMatches, 1)
-        const megtanultLecke = _.get(leckekMatches, 2)
-        const trukk = $('tr:nth-child(1) > td > font > b > div > span:nth-child(2)').text()
-        const tanulasProgressInfo = (() => {
-          if (megtanultLecke && osszesLecke) {
-            return ` (${trukk}: ${megtanultLecke}/${osszesLecke})`
-          }
-          return ''
-        })()
-        return pusher.noteAsync(null, 'Teve-o-mata', `${teve.login} megetetve és tanítva.${tanulasProgressInfo}`)
-      })
-    }
-    return pusher.noteAsync(null, 'Teve-o-mata', `${teve.login} megetetve. Válassz új trükköt neki!`)
-  }).catch((err) => {
-    log.error(err)
-    return pusher.noteAsync(null, 'Teve-o-mata', err)
   })
+}
+
+function pushTeachingInfo(teveclubRestClient, teve) {
+  return teveclubRestClient.getAsync('/tanit.pet').spread((response, body) => {
+    const $ = cheerio.load(body)
+    const leckekRawText = $('tr:nth-child(1) > td > font > b > div').text()
+    const leckekMatches = leckekRawText.match(new RegExp('(\\d+)[^\\d]+Ebb.l tev.d m.r[^\\d]+(\\d+)'))
+    const osszesLecke = _.get(leckekMatches, 1)
+    const megtanultLecke = _.get(leckekMatches, 2)
+    const trukk = $('tr:nth-child(1) > td > font > b > div > span:nth-child(2)').text()
+    const tanulasProgressInfo = (() => {
+      if (megtanultLecke && osszesLecke) {
+        return ` (${trukk}: ${megtanultLecke}/${osszesLecke})`
+      }
+      return ' Holnap új trükköt tanulhat!'
+    })()
+    return pusher.noteAsync(null, 'Teve-o-mata', `${teve.login} megetetve és tanítva.${tanulasProgressInfo}`)
+  })
+}
+
+function startNewTeaching(teveclubRestClient, teve) {
+  // TODO choose new trick automatically
+  return pusher.noteAsync(null, 'Teve-o-mata', `${teve.login} megetetve. Válassz új trükköt neki!`)
+}
+
+return Promise.map(teveList, (teve) => {
+  const teveclubRestClient = new TeveclubRestClient()
+
+  return init(teveclubRestClient).then(() => {
+    return login(teveclubRestClient, teve)
+  }).then(() => {
+    return feed(teveclubRestClient)
+  }).then(() => {
+    return continueTeaching(teveclubRestClient)
+  }).then((isTeachingSuccessful) => {
+    if (isTeachingSuccessful) {
+      return pushTeachingInfo(teveclubRestClient, teve)
+    }
+    return startNewTeaching(teveclubRestClient, teve)
+  })
+}).catch((err) => {
+  log.error(err)
+  return pusher.noteAsync(null, 'Teve-o-mata', err)
 })
